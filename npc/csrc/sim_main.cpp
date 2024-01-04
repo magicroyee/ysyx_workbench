083445e35692e1e199c067db09278567062c4d74
tracer-ysyx <tracer@ysyx.org>
2024-01-04 11:25:57 +0800

    > sim RTL ysyx_22040000 朱博宇 Linux dell-PowerEdge-R750 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:25:57 up 1 day, 13:49,  3 users,  load average: 0.91, 0.58, 0.42

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -10,27 +10,27 @@ static TOP_NAME dut;
 
 void nvboard_bind_all_pins(Vtop *top);
 
-static void single_cycle() {
-  dut.clk = 0; dut.eval();
-  dut.clk = 1; dut.eval();
-}
+// static void single_cycle() {
+//   dut.clk = 0; dut.eval();
+//   dut.clk = 1; dut.eval();
+// }
 
-static void reset(int n) {
-  dut.rstn = 0;
-  while (n -- > 0) single_cycle();
-  dut.rstn = 1;
-}
+// static void reset(int n) {
+//   dut.rstn = 0;
+//   while (n -- > 0) single_cycle();
+//   dut.rstn = 1;
+// }
 
 int main(int argc, char** argv)
 {
     nvboard_bind_all_pins(&dut);
     nvboard_init();
 
-    reset(10);
+    // reset(10);
 
     while(1) {
         nvboard_update();
-        single_cycle();
+        // single_cycle();
     }
 
     nvboard_quit();
