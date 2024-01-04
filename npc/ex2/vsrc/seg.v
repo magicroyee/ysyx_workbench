7826ffdd519b73def0ba56f3e43b8da9c731441b
tracer-ysyx <tracer@ysyx.org>
2024-01-04 21:25:59 +0800

    > sim RTL ysyx_22040000 朱博宇 Linux dell-PowerEdge-R750 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:25:59 up 1 day, 23:49,  3 users,  load average: 0.94, 0.58, 0.38

--- npc/ex2/vsrc/seg.v
+++ npc/ex2/vsrc/seg.v
@@ -10,14 +10,15 @@ module seg (
         end
         else begin
             case(data)
-            3'd0: seg = 8'b11111100;
-            3'd1: seg = 8'b01100000;
-            3'd2: seg = 8'b11011010;
-            3'd3: seg = 8'b11110010;
-            3'd4: seg = 8'b01100110;
-            3'd5: seg = 8'b10110110;
-            3'd6: seg = 8'b10111110;
-            3'd7: seg = 8'b11100000;
+            3'd0: seg = 8'b00000011;
+            3'd1: seg = 8'b10011111;
+            3'd2: seg = 8'b00100101;
+            3'd3: seg = 8'b00001101;
+            3'd4: seg = 8'b10011001;
+            3'd5: seg = 8'b01001001;
+            3'd6: seg = 8'b01000001;
+            3'd7: seg = 8'b00011111;
+            default: seg = 8'b00000000;
             endcase
         end
     end
