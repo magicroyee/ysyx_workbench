c8e318af59d9b1d5f251ab7ab7cd30ee5e1f2d10
tracer-ysyx <tracer@ysyx.org>
2024-01-04 14:11:28 +0800

    > sim RTL ysyx_22040000 朱博宇 Linux dell-PowerEdge-R750 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:11:28 up 1 day, 16:34,  3 users,  load average: 0.45, 0.49, 0.38

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -10,30 +10,31 @@ static TOP_NAME dut;
 
 void nvboard_bind_all_pins(Vtop *top);
 
-static void single_cycle() {
-  dut.clk = 0; dut.eval();
-  dut.clk = 1; dut.eval();
-}
+// static void single_cycle() {
+//   dut.clk = 0; dut.eval();
+//   dut.clk = 1; dut.eval();
+// }
 
-static void reset(int n) {
-  dut.rstn = 0;
-  while (n -- > 0) single_cycle();
-  dut.rstn = 1;
-}
+// static void reset(int n) {
+//   dut.rstn = 0;
+//   while (n -- > 0) single_cycle();
+//   dut.rstn = 1;
+// }
 
 int main(int argc, char** argv)
 {
     nvboard_bind_all_pins(&dut);
     nvboard_init();
 
-    reset(10);
+    // reset(10);
 
     while(1) {
         nvboard_update();
         dut.a = rand() & 1;
+        dut.eval();
         dut.b = rand() & 1;
         dut.eval();
-        single_cycle();
+        // single_cycle();
     }
 
     nvboard_quit();
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,16 +1,8 @@
 module top(
-    input clk,
-    input rstn,
     input a,
     input b,
-    output reg f
+    output f
 );
-    always @(posedge clk or negedge rstn) begin
-        if (!rstn) begin
-            f <= 1'b0;
-        end else begin
-            f <= a ^ b;
-        end
-    end
+    assign f = a^b;
 
 endmodule
