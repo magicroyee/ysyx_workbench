5fa4cfc69306b0c5dedf8e75f509008c5f8bc703
tracer-ysyx <tracer@ysyx.org>
2024-01-04 21:19:50 +0800

    > sim RTL ysyx_22040000 朱博宇 Linux dell-PowerEdge-R750 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:19:50 up 1 day, 23:43,  3 users,  load average: 0.41, 0.32, 0.22

---
+++ npc/ex2/vsrc/seg.v
@@ -0,0 +1,25 @@
+module seg (
+    input en,
+    input [2:0] data,
+    output [7:0] seg
+);
+
+    always @(*) begin
+        if (!en) begin
+            seg = 8'b00000000;
+        end
+        else begin
+            case(data)
+            3'd0: seg = 8'b11111100;
+            3'd1: seg = 8'b01100000;
+            3'd2: seg = 8'b11011010;
+            3'd3: seg = 8'b11110010;
+            3'd4: seg = 8'b01100110;
+            3'd5: seg = 8'b10110110;
+            3'd6: seg = 8'b10111110;
+            3'd7: seg = 8'b11100000;
+            endcase
+        end
+    end
+
+endmodule
