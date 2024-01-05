d2a68c983b48587a76a70dc14c767331f7e88732
tracer-ysyx <tracer@ysyx.org>
2024-01-05 21:22:16 +0800

    > sim RTL ysyx_22040000 朱博宇 Linux dell-PowerEdge-R750 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:22:16 up 2 days, 23:45,  4 users,  load average: 1.49, 1.45, 1.40

---
+++ npc/ex7/csrc/sim_main.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Vtop.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+u_int8_t din[] = {0x00, 0x01, 0x11, 0x55, 0x99, 0xff};
+
+VerilatedContext *contextp = NULL;
+VerilatedVcdC *tfp = NULL;
+static Vtop *top = NULL;
+
+void step_and_dump_wave()
+{
+    top->eval();
+    contextp->timeInc(1);
+    tfp->dump(contextp->time());
+}
+
+// void single_cycle()
+// {
+//     top->clk = 0;
+//     step_and_dump_wave();
+//     top->clk = 1;
+//     step_and_dump_wave();
+// }
+
+// void reset(int n)
+// {
+//     top->rstn = 0;
+//     while (n-- > 0)
+//         single_cycle();
+//     top->rstn = 1;
+// }
+
+int main(int argc, char **argv)
+{
+    contextp = new VerilatedContext;
+    tfp = new VerilatedVcdC;
+    top = new Vtop{contextp};
+    contextp->commandArgs(argc, argv);
+
+    contextp->traceEverOn(true);
+    top->trace(tfp, 0);
+    tfp->open("./sim/wave.vcd");
+
+    reset(10);
+
+    while (!contextp->gotFinish())
+    {
+        step_and_dump_wave();
+    }
+
+    delete top;
+    tfp->close();
+    delete contextp;
+    return 0;
+}
---
+++ npc/ex7/vsrc/top.v
@@ -0,0 +1,7 @@
+module top(
+    
+);
+
+    ps2_keyboard u_ps2_keyboard ();
+
+endmodule
