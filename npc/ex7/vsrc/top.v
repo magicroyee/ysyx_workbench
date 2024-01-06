0b6803c04bd710823ebeb315ca006ae8dbf96b57
tracer-ysyx <tracer@ysyx.org>
2024-01-06 15:43:14 +0800

    > run RTL ysyx_22040000 朱博宇 Linux dell-PowerEdge-R750 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:43:14 up 3 days, 18:06,  4 users,  load average: 2.45, 2.32, 2.34

--- npc/ex7/vsrc/top.v
+++ npc/ex7/vsrc/top.v
@@ -1,9 +1,31 @@
-`timescale 1ns / 1ps
+// module ps2_keyboard(clk,clrn,ps2_clk,ps2_data,data,
+//                     ready,nextdata_n,overflow);
+//     input clk,clrn,ps2_clk,ps2_data;
+//     input nextdata_n;
+//     output [7:0] data;
+//     output reg ready;
+//     output reg overflow;     // fifo overflow
 module top(
-    
+    input clk,
+    input rstn,
+    input ps2_clk,
+    input ps2_data,
+    output [7:0] data,
+    output ready,
+    output nextdata_n,
+    output overflow
 );
 
-    keyboard_sim u_keyboard_sim (
+    ps2_keyboard inst(
+        .clk(clk),
+        .clrn(rstn),
+        .ps2_clk(ps2_clk),
+        .ps2_data(ps2_data),
+        .data(data),
+        .ready(ready),
+        .nextdata_n(nextdata_n),
+        .overflow(overflow)
     );
+
 
 endmodule
