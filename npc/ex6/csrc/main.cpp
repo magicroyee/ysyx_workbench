1de20f6cfee7bd85400d8fb6c559f7154ed87fce
tracer-ysyx <tracer@ysyx.org>
2024-01-12 21:35:19 +0800

    > run RTL ysyx_22040000 朱博宇 Linux ysyx-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:35:19 up  1:17,  1 user,  load average: 0.52, 0.56, 0.53

--- npc/ex6/csrc/main.cpp
+++ npc/ex6/csrc/main.cpp
@@ -12,8 +12,9 @@ static TOP_NAME dut;
 void nvboard_bind_all_pins(Vtop *top);
 
 static void single_cycle() {
-    dut.clk = 0; dut.eval();
-    dut.clk = 1; dut.eval();
+    // dut.clk = 0; dut.eval();
+    // dut.clk = 1; dut.eval();
+    dut.eval();
 }
 
 static void reset(int n) {
