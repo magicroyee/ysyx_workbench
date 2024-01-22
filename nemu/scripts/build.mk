f89dd30173ef76bd738694641348c8ba52efd926
tracer-ysyx <tracer@ysyx.org>
2024-01-22 11:06:45 +0800

    > compile NEMU ysyx_23060180 朱博宇 Linux dell-PowerEdge-R750 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:06:45 up 19 days, 13:30,  0 users,  load average: 4.38, 5.87, 21.58

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -31,7 +31,7 @@ OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 $(OBJ_DIR)/%.o: %.c
 	@echo + CC $<
 	@mkdir -p $(dir $@)
-	@$(CC) $(CFLAGS) -c -o $@ $<
+	$(CC) $(CFLAGS) -c -o $@ $<
 	@$(CC) $(CFLAGS) -E -o $@.i $<
 	$(call call_fixdep, $(@:.o=.d), $@)
 
