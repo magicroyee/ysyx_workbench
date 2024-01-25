53ea843f89f9eeac07d6a17a8bff4b892d8922e0
Zhu Boyu <1300874867@qq.com>
2024-01-25 22:16:42 +0800

    pa2.3 function about string and memory

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,7 +9,14 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  int i = 0;
+  while (src[i] != '\0') {
+    dst[i] = src[i];
+    i++;
+  }
+  dst[i] = '\0';
+  
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +24,34 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  int i = 0;
+  while (dst[i] != '\0') {
+    i++;
+  }
+  int j = 0;
+  while (src[j] != '\0') {
+    dst[i] = src[j];
+    i++;
+    j++;
+  }
+  dst[i] = '\0';
+
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  int i = 0;
+  while (s1[i] != '\0' || s2[i] != '\0') {
+    if (s1[i] != s2[i]) {
+      return s1[i] - s2[i];
+    }
+    if (s1[i] == '\0' || s2[i] == '\0') {
+      return s1[i] - s2[i];
+    }
+    i++;
+  }
+
+  return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +59,11 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  for (int i = 0; i < n; i++) {
+    ((char *)s)[i] = c;
+  }
+
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -37,11 +71,21 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  for (int i = 0; i < n; i++) {
+    ((char *)out)[i] = ((char *)in)[i];
+  }
+
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  for (int i = 0; i < n; i++) {
+    if (((char *)s1)[i] != ((char *)s2)[i]) {
+      return ((char *)s1)[i] - ((char *)s2)[i];
+    }
+  }
+
+  return 0;
 }
 
 #endif
