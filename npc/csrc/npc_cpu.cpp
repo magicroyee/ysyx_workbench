004c25d7b6d6ed2f8ec605d4158fd21edbebe799
Zhu Boyu <zhuboyubit@163.com>
2024-04-27 21:38:51 +0800

    itrace for npc

--- npc/csrc/npc_cpu.cpp
+++ npc/csrc/npc_cpu.cpp
@@ -1,24 +1,50 @@
-#include <npc_cpu.h>
+#include "npc_cpu.h"
+#include "npc_config.h"
 #include "npc_init.h"
 #include "npc_memory.h"
 #include "hardware.h"
 
-// u_int32_t mem_rd = 0;
-// u_int32_t mem_raddr = 0;
+char log_buf[LOG_BUF_LEN];
 
-// #define EXEC_CHECK_END { \
-//     if (npc_state.state != NPC_RUNNING) { \
-//         return; \
-//     } \
-// }
+static void trace()
+{
+    #ifdef ITRACE
+    printf("%s\n", log_buf);
+    #endif
+}
 
 static void exec_once() {
+    uint32_t pc = CPU_PC;
+    uint32_t instval = vaddr_read(pc, 4);
+    char *p = log_buf;
+    p += snprintf(p, sizeof(log_buf), "pc: 0x%08x:", CPU_PC);
+
+    int ilen = 4;
+    uint8_t *inst = (uint8_t *)&instval;
+    for (int i = ilen - 1; i >= 0; i--) {
+        p += snprintf(p, 4, " %02x", inst[i]);
+    }
+
+    int ilen_max = 4;
+    int space_len = ilen_max - ilen;
+    if (space_len < 0) {
+        space_len = 0;
+    }
+
+    space_len = space_len * 3 + 1;
+    memset(p, ' ', space_len);
+    p += space_len;
+
+    void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+    disassemble(p, log_buf + sizeof(log_buf) - p, pc, (uint8_t *)&instval, ilen);
+
     isa_exec_once();
 }
 
 static void execute(uint32_t n) {
     for (uint32_t i = 0; i < n; i++) {
         exec_once();
+        trace();
         if (npc_state.state != NPC_RUNNING) {
             break;
         }
