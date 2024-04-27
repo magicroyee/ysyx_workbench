e390ba8c34d2e08f091f4d854bcd272146c555c4
tracer-ysyx <tracer@ysyx.org>
2024-04-27 17:58:55 +0800

    > sim RTL ysyx_23060180 朱博宇 Linux ysyx-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:58:55 up  1:24,  1 user,  load average: 1.15, 0.80, 0.71

--- npc/csrc/utils/disasm.cc
+++ npc/csrc/utils/disasm.cc
@@ -96,16 +96,11 @@ void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
   MCInst inst;
   llvm::ArrayRef<uint8_t> arr(code, nbyte);
   uint64_t dummy_size = 0;
-  printf("init success\n");
   gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
-
-  printf("get success\n");
 
   std::string s;
   raw_string_ostream os(s);
   gIP->printInst(&inst, pc, "", *gSTI, os);
-
-  printf("printInst success\n");
 
   int skip = s.find_first_not_of('\t');
   const char *p = s.c_str() + skip;
