e7bf8f0d19eaef24523c1c1c591503673022da28
tracer-ysyx <tracer@ysyx.org>
2024-06-18 21:51:55 +0800

    > sim RTL ysyx_23060180 朱博宇 Linux ysyx-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:51:55 up 12:22,  1 user,  load average: 0.55, 0.36, 0.44

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -26,6 +26,6 @@ image: $(IMAGE).elf
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: image
-	@echo $(NPCFLAGS)
+	@echo "$(NPCFLAGS)"
 	$(MAKE) -C $(NPC_HOME) sim ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin
 	
