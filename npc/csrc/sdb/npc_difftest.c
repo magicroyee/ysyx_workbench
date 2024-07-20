8ee1d1749cb162556ca819573aab8207818333a4
tracer-ysyx <tracer@ysyx.org>
2024-07-20 16:43:45 +0800

    > sim RTL ysyx_23060180 朱博宇 Linux ysyx-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:43:45 up 19:02,  1 user,  load average: 1.92, 0.96, 0.80

--- npc/csrc/sdb/npc_difftest.c
+++ npc/csrc/sdb/npc_difftest.c
@@ -29,6 +29,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
     // ref_difftest_raise_intr = dlsym(handle, "difftest_raise_intr");
     // assert(ref_difftest_raise_intr);
     isa_reg_read();
+    printf("cpu.pc: 0x%08x\n", cpu.pc);
 
     ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
