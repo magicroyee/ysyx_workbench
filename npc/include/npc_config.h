9216d8a83dfec4111c9ebffd765f9319f73ec8d6
tracer-ysyx <tracer@ysyx.org>
2024-07-21 14:25:29 +0800

    > sim RTL ysyx_23060180 朱博宇 Linux ysyx-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:25:29 up  5:17,  1 user,  load average: 0.96, 0.67, 0.48

--- npc/include/npc_config.h
+++ npc/include/npc_config.h
@@ -1,10 +1,10 @@
 #ifndef __NPC_CONFIG_H__
 #define __NPC_CONFIG_H__
 
-#define ITRACE  1
-#define MTRACE  1
-#define FTRACE  1
+// #define ITRACE  1
+// #define MTRACE  1
+// #define FTRACE  1
 
-#define DIFF_TEST 1
+// #define DIFF_TEST 1
 
 #endif
