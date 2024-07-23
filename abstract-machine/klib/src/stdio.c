ea1613986ec840428dcb5f3171360efab2e6a4a8
tracer-ysyx <tracer@ysyx.org>
2024-07-23 20:29:01 +0800

    > compile NEMU ysyx_23060180 朱博宇 Linux ysyx-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:29:01 up 1 day,  1:24,  1 user,  load average: 0.55, 0.55, 0.54

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -70,53 +70,11 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 
 int sprintf(char *out, const char *fmt, ...) {
   va_list ap;
-  int d;
-  char c, *s;
-  char *p = out;
 
   va_start(ap, fmt);
-  while (*fmt) {
-    if (*fmt != '%') {
-      *p++ = *fmt++;
-      continue;
-    }
-    fmt++;
-    switch (*fmt) {
-      case 's':
-        s = va_arg(ap, char *);
-        while (*s) {
-          *p++ = *s++;
-        }
-        break;
-      case 'd':
-        d = va_arg(ap, int);
-        if (d < 0) {
-          *p++ = '-';
-          d = -d;
-        }
-        char buf[32];
-        int i = 0;
-        do {
-          buf[i++] = d % 10 + '0';
-          d /= 10;
-        } while (d);
-        while (i--) {
-          *p++ = buf[i];
-        }
-        break;
-      case 'c':
-        c = va_arg(ap, int);
-        *p++ = c;
-        break;
-      default:
-        break;
-    }
-    fmt++;
-  }
-
-  *p = '\0';
+  int n = vsprintf(out, fmt, ap);
   va_end(ap);
-  return p - out;
+  return n;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
