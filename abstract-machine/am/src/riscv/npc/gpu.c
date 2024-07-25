fcf8dcd3398560980eefa35499ce95a5c771e73f
Zhu Boyu <zhuboyubit@163.com>
2024-07-25 17:24:13 +0800

    add vga for npc

---
+++ abstract-machine/am/src/riscv/npc/gpu.c
@@ -0,0 +1,39 @@
+#include <am.h>
+#include <npc.h>
+#include <klib.h>
+
+#define SYNC_ADDR (VGACTL_ADDR + 4)
+
+void __am_gpu_init() {
+}
+
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  *cfg = (AM_GPU_CONFIG_T) {
+    .present = true, .has_accel = false,
+    .width = inw(VGACTL_ADDR+2), .height = inw(VGACTL_ADDR),
+    .vmemsz = 0
+  };
+}
+
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  uint32_t *pixels = ctl->pixels;
+  // int cp_bytes = sizeof(uint32_t) * w;
+  // for (int j = 0; j < h; j ++) {
+  //   memcpy(&fb[(y + j) * 400 + x], pixels, cp_bytes);
+  //   pixels += w;
+  // }
+  for (int j = 0; j < h; j ++) {
+    for (int i = 0; i < w; i ++) {
+      fb[(y + j) * 400 + x + i] = pixels[j * w + i];
+    }
+  }
+  if (ctl->sync) {
+    outl(SYNC_ADDR, 1);
+  }
+}
+
+void __am_gpu_status(AM_GPU_STATUS_T *status) {
+  status->ready = true;
+}
